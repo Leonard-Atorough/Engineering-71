a6afede94f6d01bed8a56bb9da13d1c4d1e22d22
Leonard Atorough <leotorough@gmail.com>
2020-11-16 16:07:45 +0000

    Added collections and data types notes

---
+++ core/Lab09_Collections/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab09_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* var numbers = new List<int> {11, 31, -4, 6, 0 };
+
+            numbers.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            numbers.Add(24);
+            numbers.Add(-45);
+            numbers.Add(3);
+
+            numbers[2] = 4;
+
+            numbers.Reverse();
+
+            numbers.RemoveAt(3);
+
+            numbers.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            numbers.Sort();
+
+            numbers.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine(); */
+
+
+            // QUEUE FIRST IN, FIRST OUT
+            //var myQueue = new Queue<string>();
+
+            //myQueue.Enqueue("Will");
+            //myQueue.Enqueue("Arthur");
+            //myQueue.Enqueue("Bill");
+
+            //Console.WriteLine(myQueue.Dequeue());
+
+            //foreach (string item in myQueue)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            // STACK LAST IN, FIRST OUT
+
+            //var myStack = new Stack<string>();
+
+            //myStack.Push("Chair 1");
+            //myStack.Push("Chair 2");
+            //myStack.Push("Chair 3");
+
+            //foreach (string item in myStack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //myStack.Pop();
+
+            //foreach (string item in myStack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // HashSet<T> - unordered collections
+
+            //var eng71 = new HashSet<string> {"Nish", "Paula", "Lee", "Cathy", "Lee"};
+
+            //var count = eng71.Count;
+            //Console.WriteLine(count);
+
+
+            //foreach (string item in eng71)
+            //{
+            //    Console.WriteLine(item);
+            //}        
+
+            //DICTIONARY
+
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+
+            //myDictionary.Add(1, "Paula");
+            //myDictionary.Add(2, "David");
+            //myDictionary.Add(3, "Lee");
+            //myDictionary.TryAdd(3, "Leonard");
+
+            //foreach (var item in myDictionary)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var dictionar2020 = new Dictionary<char, string> { { 'a', "Aligator" }, { 'b', "Baboon" } };
+
+            //var p = dictionar2020['a'];
+
+            //Console.WriteLine(p);
+
+            //foreach (var key in dictionar2020.Keys)
+            //{
+            //    Console.WriteLine($"The key is {key} and the value is {dictionar2020[key]}");
+            //}
+
+            string input = "The quick brown fox jumps over the lazy dog";
+
+            var dictionary = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (dictionary.ContainsKey(c))
+                {
+                    dictionary[c]++;
+                }
+
+                else
+                {
+                    dictionary.Add(c, 1);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"there are {item.Value} instances of {item.Key} in the string");
+            }
+        }
+    }
+
+}
---
+++ core/Lab10_Datatypes_2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab10_Datatypes_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DATETIME & TIMESPAN
+
+            //var tps = TimeSpan.TicksPerSecond;
+            //var nowInTicks = DateTime.Now.Ticks;
+
+            //var d = new DateTime();
+            //var d1 = DateTime.Today;
+            //var d2 = DateTime.Now;
+            //var d3 = new DateTime(1996, 11, 26, 6, 0, 0);
+
+            //var age = d2 - d3;
+            //var result = (age.Days / 365.25);
+
+            //Console.WriteLine(Math.Floor(result));
+
+            //var timespan = new TimeSpan(1, 0, 0);
+            //var d4 = new DateTime(1996, 11, 26, 6, 0, 0);
+            //var date = d4 + timespan;
+
+            //var stopwatch = new Stopwatch();
+
+            //stopwatch.Start();
+            //int total = 0;
+
+            //for (int i = 0; i <= 100000; i++)
+            //{
+            //    total++;
+            //}
+
+            //stopwatch.Stop();
+
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            //Console.WriteLine(stopwatch.ElapsedTicks);
+            //Console.WriteLine(stopwatch.Elapsed);
+            //Console.ReadLine();
+
+            //Suit newSuit = (Suit)20;
+
+            //var thisSuit = newSuit.ToString();
+            //Suits(newSuit);
+
+            //var monday = 1;
+            //var day = (DayOfWeek)monday;
+
+            //Console.WriteLine(day);
+
+            //int dog;
+            //float cat;
+            //char c;
+            //DateTime aDtae;
+            //string aString;
+            //bool thing;
+            //int[] intarray;
+
+            //int sparts;
+            //int? globs;
+
+            //    bool? hasPassed = true;
+
+            //    if (hasPassed.HasValue)
+            //    {
+            //        Console.WriteLine($"hasPassed has a value of {hasPassed}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Has passed has a value of null");
+            //    }
+
+            //    int totalCost = 0;
+            //    int price = 5;
+
+            //    int? items = null;
+
+            //    if (items.HasValue)
+            //    {
+            //        totalCost = price * items.Value;
+            //    }
+            //    Console.WriteLine(items.HasValue);
+            //    Console.WriteLine(totalCost);
+            // 
+
+
+            // const
+            // const int dozen = 12;
+
+            // dynamic allows us to break strong typing. Don't use
+
+            //dynamic item = 100;
+
+            //Console.WriteLine($"Adding 10 to {item} gives {item + 10}");
+
+            //item = "Hello";
+            //Console.WriteLine($"Adding 10 to {item} gives {item + 10}");
+
+            // Random class
+
+            //var rng = new Random();
+            //var rngSeeded = new Random(42);
+
+            //var between1And10 = rngSeeded.Next(1, 11);
+
+            //Console.WriteLine(between1And10);
+
+            var rollTheDice = new Random();
+
+            var dice1 = rollTheDice.Next(7);
+            var dice2 = rollTheDice.Next(7);
+            var dice3 = rollTheDice.Next(7);
+
+        }
+
+        public static void Suits (Suit suit)
+        {
+            switch (suit)
+            {
+                case Suit.HEARTS:
+                    Console.WriteLine("You have stolen a Heart!");
+                    break;
+                case Suit.DIAMONDS:
+                    Console.WriteLine("You have stolen a Diamond!");
+                    break;
+                case Suit.CLUBS:
+                    Console.WriteLine("You have stolen a Club!");
+                    break;
+                case Suit.SPADES:
+                    Console.WriteLine("You have stolen a Spade!");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public enum Suit
+        {
+            HEARTS = 20, DIAMONDS, CLUBS, SPADES
+        }
+    }
+}
